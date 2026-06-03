a010f846d1fd8417f935925feaab8105262c86aa
ngocchien5596 <buingocchien5596@gmail.com>
2026-06-03 09:22:21 +0700

    chore: remove tracked local artifacts

--- .tmp_versioncheck.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using StationApp.Application.Services;
-Console.WriteLine(AppUpdateVersionComparer.Compare("1.0.0 0601", "1.0.0 0101"));
-Console.WriteLine(AppUpdateVersionComparer.NormalizeString("1.0.0 0601"));
-Console.WriteLine(AppUpdateVersionComparer.NormalizeString("1.0.0 0101"));
